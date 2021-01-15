c132087edf6493c216db9d1d9f777bfef0ca55fc
matheusdrade <andradecmatheus@gmail.com>
2021-01-15 12:02:37 -0300

    Second commit: it adds the four basic mathematical  operations

--- Aula1-Hello_django/core/views.py
+++ Aula1-Hello_django/core/views.py
@@ -6,5 +6,14 @@ from django.shortcuts import render, HttpResponse
 def hello(request, nome, idade):
     return HttpResponse('<h1>Hello {} de {} anos</h1>'.format(nome, idade))
 
+def soma(request, a, b):
+    return HttpResponse('<h1>A soma de {} + {} é {}</h1>'.format(a, b, a+b))
 
+def sub(request, a, b):
+    return HttpResponse('<h1>A subtração de {} - {} é {}</h1>'.format(a, b, a-b))
 
+def mult(request, a, b):
+    return HttpResponse('<h1>A multiplicação de {} * {} é {}</h1>'.format(a, b, a*b))
+
+def div(request, a, b):
+    return HttpResponse('<h1>A divisão de {} / {} é {}</h1>'.format(a, b, a/b))
