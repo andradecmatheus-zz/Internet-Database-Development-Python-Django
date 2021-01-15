efe7412bb8482cd72301bfc169a7c8b58a70b9d6
matheusdrade <andradecmatheus@gmail.com>
2021-01-15 15:51:18 -0300

    Event table created and filters for to work with Agenda

--- Aula2-Agenda/core/models.py
+++ Aula2-Agenda/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True) #pode ficar em branco e ser nulo
+    data_evento = models.DateTimeField(verbose_name='Data do Evento') #configura a label
+    data_criacao = models.DateTimeField(auto_now=True) #inseri automaticamente
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE) #cascade: se o usuário for escluido da app, tudo dele tbm será
+
+    class Meta:
+        db_table = 'evento' #exige q o nome da tabela será evento)
+
+    def __str__(self):#sempre q alguem chamar o objeto título, será trazido o nome do título,
+        return self.titulo #mesmo q ñ seja clicado p/ acessar algum campo do evento (evento object ñ mais)
+
