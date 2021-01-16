84ee63e8d3e7422cbb48a9a6e7343d96c73dab6e
matheusdrade <andradecmatheus@gmail.com>
2021-01-16 13:44:59 -0300

    Events insertion template and gitignore were included

--- Aula2-Agenda/core/views.py
+++ Aula2-Agenda/core/views.py
@@ -34,3 +34,20 @@ def lista_eventos(request):
     #evento = Evento.objects.all()
     dados = {'eventos':evento} #evento agora é no plural
     return render(request, 'agenda.html', dados)
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario) #na listagem era um objects.filter, aqui é um objects.create
+    return redirect('/')
